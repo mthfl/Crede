b01d9845af250eb9daf2b24394e77d9e07da2aa6
mth <matheus.dev91@gmail.com>
2025-09-09 15:35:57 -0300

    fix: adjust archivs

--- app/subsystems/controle_de_estoque/app/main/config/connect.php
+++ app/subsystems/controle_de_estoque/app/main/config/connect.php
@@ -2,32 +2,33 @@
 class connect
 {
     protected $connect;
-    function __construct($escola)
+
+    function __construct()
     {
-        $this->connect_database($escola);
+        $this->connect_database();
     }
 
-    function connect_database($escola)
+    function connect_database()
     {
         try {
-            $config = require(__DIR__."/../../../.env/config.php");
+            $config = require(__DIR__."/../models/private/config.php");
 
             // Tentar primeiro o banco local
             try {
-                $host = $config['local']["ss_$escola"]['host'];
-                $database = $config['local']["ss_$escola"]['banco'];
-                $user = $config['local']["ss_$escola"]['user'];
-                $password = $config['local']["ss_$escola"]['senha'];
+                $host = $config['local']['crede_estoque']['host'];
+                $database = $config['local']['crede_estoque']['banco'];
+                $user = $config['local']['crede_estoque']['user'];
+                $password = $config['local']['crede_estoque']['senha'];
 
                 $this->connect = new PDO('mysql:host=' . $host . ';dbname=' . $database . ';charset=utf8', $user, $password);
                 $this->connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 $this->connect->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
             } catch (PDOException $e) {
                 // Se falhar, tentar o banco da hospedagem
-                $host = $config['hospedagem']["ss_$escola"]['host'];
-                $database = $config['hospedagem']["ss_$escola"]['banco'];
-                $user = $config['hospedagem']["ss_$escola"]['user'];
-                $password = $config['hospedagem']["ss_$escola"]['senha'];
+                $host = $config['hospedagem']['crede_estoque']['host'];
+                $database = $config['hospedagem']['crede_estoque']['banco'];
+                $user = $config['hospedagem']['crede_estoque']['user'];
+                $password = $config['hospedagem']['crede_estoque']['senha'];
 
                 $this->connect = new PDO('mysql:host=' . $host . ';dbname=' . $database . ';charset=utf8', $user, $password);
                 $this->connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -35,10 +36,10 @@ class connect
             }
         } catch (PDOException $e) {
 
-            /*error_log("Erro de conexão com banco: " . $e->getMessage());
+            error_log("Erro de conexão com banco: " . $e->getMessage());
             $this->connect = null;
             header('location:../views/windows/desconnect.php');
-            exit();*/
+            exit();
         }
     }
 }
---
+++ app/subsystems/controle_de_estoque/app/main/controllers/controller_solicitar.php
@@ -0,0 +1,78 @@
+<?php
+require_once(__DIR__ . '/../models/sessions.php');
+$session = new sessions();
+$session->autenticar_session();
+$session->tempo_session();
+
+require_once(__DIR__ . '/../models/model.usuario.php');
+$select = new usuario();
+//print_r($_POST);
+
+if (
+    isset($_POST['opcao_atual']) && $_POST['opcao_atual'] == 'select' &&
+    isset($_POST['produto']) && !empty($_POST['produto']) && is_numeric($_POST['produto']) &&
+    isset($_POST['retirante']) && !empty($_POST['retirante']) && is_numeric($_POST['retirante']) &&
+    isset($_POST['quantidade']) && !empty($_POST['quantidade']) && is_numeric($_POST['quantidade'])
+) {
+
+    $id_produto = $_POST['produto'];
+    $retirante = $_POST['retirante'];
+    $valor_retirada = $_POST['quantidade'];
+    $usuario = $_SESSION['nome'];
+    $model = new usuario();
+
+    date_default_timezone_set('America/Fortaleza');
+    $datatime = date('Y-m-d H:i:s');
+    $result = $model->solicitar_produto_id($valor_retirada, $id_produto, $retirante, $datatime, $usuario);
+
+    switch ($result) {
+        case 1:
+            header("Location: ../views/solicitar.php?retirado");
+            break;
+        case 2:
+            header("Location: ../views/solicitar.php?erro");
+            break;
+        case 3:
+            header("Location: ../views/solicitar.php?sem_produtos");
+            break;
+        case 4:
+            header("Location: ../views/solicitar.php?numero_alto");
+            break;
+        default:
+            header("Location: ../views/solicitar.php?fatal");
+            break;
+    }
+} else if (
+    isset($_POST['barcode']) && !empty($_POST['barcode']) && is_numeric($_POST['barcode']) &&
+    isset($_POST['retirante']) && !empty($_POST['retirante']) && is_numeric($_POST['retirante']) &&
+    isset($_POST['quantidade']) && !empty($_POST['quantidade']) && is_numeric($_POST['quantidade'])
+) {
+
+    $id_produto = $_POST['barcode'];
+    $retirante = $_POST['retirante'];
+    $valor_retirada = $_POST['quantidade'];
+    $usuario = $_SESSION['nome'];
+    $model = new usuario();
+
+    date_default_timezone_set('America/Fortaleza');
+    $datatime = date('Y-m-d H:i:s');
+    $result = $model->solicitar_produto_barcode($valor_retirada, $id_produto, $retirante, $datatime, $usuario);
+
+    switch ($result) {
+        case 1:
+            header("Location: ../views/solicitar.php?retirado");
+            break;
+        case 2:
+            header("Location: ../views/solicitar.php?erro");
+            break;
+        case 3:
+            header("Location: ../views/solicitar.php?sem_produtos");
+            break;
+        case 4:
+            header("Location: ../views/solicitar.php?numero_alto");
+            break;
+        default:
+            header("Location: ../views/solicitar.php?fatal");
+            break;
+    }
+}
