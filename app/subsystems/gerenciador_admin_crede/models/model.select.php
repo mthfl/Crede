21154c6f049ea7e22d860dbd673610bab2feaf33
Felipe Gomes <fgmss0907@gmail.com>
2025-10-05 16:33:44 -0300

    feat: adjusts general admin

--- app/subsystems/gerenciador_admin_crede/models/model.select.php
+++ app/subsystems/gerenciador_admin_crede/models/model.select.php
@@ -91,4 +91,80 @@ class select extends connect
         return $ercr;
     }
 
+    public function insert_user(string $epKey, string $nome, string $email, string $cpf): bool {
+        $map = [
+            'estgdm' => [$this->connect_estgdm, $this->table1],
+            'epaf'   => [$this->connect_epaf, $this->table2],
+            'epmfm'  => [$this->connect_epmfm, $this->table3],
+            'epav'   => [$this->connect_epav, $this->table4],
+            'eedq'   => [$this->connect_eedq, $this->table5],
+            'ejin'   => [$this->connect_ejin, $this->table6],
+            'epfads' => [$this->connect_epfads, $this->table7],
+            'emcvm'  => [$this->connect_emcvm, $this->table8],
+            'eglgfm' => [$this->connect_eglgfm, $this->table9],
+            'epldtv' => [$this->connect_epldtv, $this->table10],
+            'ercr'   => [$this->connect_ercr, $this->table11],
+        ];
+
+        if (!isset($map[$epKey])) {
+            throw new InvalidArgumentException('Escola inválida');
+        }
+
+        [$pdo, $table] = $map[$epKey];
+        $stmt = $pdo->prepare("INSERT INTO $table (nome_user, email, cpf, tipo_usuario) VALUES (:nome, :email, :cpf, 'admin')");
+        $stmt->bindValue(':nome', $nome);
+        $stmt->bindValue(':email', $email);
+        $stmt->bindValue(':cpf', $cpf);
+        return $stmt->execute();
+    }
+
+    public function update_user(string $epKey, int $id, string $nome, string $email, string $cpf): bool {
+        $map = [
+            'estgdm' => [$this->connect_estgdm, $this->table1],
+            'epaf'   => [$this->connect_epaf, $this->table2],
+            'epmfm'  => [$this->connect_epmfm, $this->table3],
+            'epav'   => [$this->connect_epav, $this->table4],
+            'eedq'   => [$this->connect_eedq, $this->table5],
+            'ejin'   => [$this->connect_ejin, $this->table6],
+            'epfads' => [$this->connect_epfads, $this->table7],
+            'emcvm'  => [$this->connect_emcvm, $this->table8],
+            'eglgfm' => [$this->connect_eglgfm, $this->table9],
+            'epldtv' => [$this->connect_epldtv, $this->table10],
+            'ercr'   => [$this->connect_ercr, $this->table11],
+        ];
+        if (!isset($map[$epKey])) {
+            throw new InvalidArgumentException('Escola inválida');
+        }
+        [$pdo, $table] = $map[$epKey];
+        $stmt = $pdo->prepare("UPDATE $table SET nome_user = :nome, email = :email, cpf = :cpf WHERE id = :id");
+        $stmt->bindValue(':nome', $nome);
+        $stmt->bindValue(':email', $email);
+        $stmt->bindValue(':cpf', $cpf);
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+        return $stmt->execute();
+    }
+
+    public function delete_user(string $epKey, int $id): bool {
+        $map = [
+            'estgdm' => [$this->connect_estgdm, $this->table1],
+            'epaf'   => [$this->connect_epaf, $this->table2],
+            'epmfm'  => [$this->connect_epmfm, $this->table3],
+            'epav'   => [$this->connect_epav, $this->table4],
+            'eedq'   => [$this->connect_eedq, $this->table5],
+            'ejin'   => [$this->connect_ejin, $this->table6],
+            'epfads' => [$this->connect_epfads, $this->table7],
+            'emcvm'  => [$this->connect_emcvm, $this->table8],
+            'eglgfm' => [$this->connect_eglgfm, $this->table9],
+            'epldtv' => [$this->connect_epldtv, $this->table10],
+            'ercr'   => [$this->connect_ercr, $this->table11],
+        ];
+        if (!isset($map[$epKey])) {
+            throw new InvalidArgumentException('Escola inválida');
+        }
+        [$pdo, $table] = $map[$epKey];
+        $stmt = $pdo->prepare("DELETE FROM $table WHERE id = :id");
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+        return $stmt->execute();
+    }
+
 }
